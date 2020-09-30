016b7665263c4930b84ca6c43496ffefd03494c5
Sarwar457 <gulam.sarwar5@gmail.com>
2020-09-30 21:00:19 +0530

    file moved to central repository.

---
+++ MavenApp/src/test/java/com/cg/project/AppTest1.java
@@ -0,0 +1,21 @@
+package com.cg.project;
+
+import org.junit.Test;
+
+public class AppTest1 {
+	@Test
+	public void test1 () {
+		System.out.println("test1");
+	}
+	
+	@Test
+	public void test2 () {
+		System.out.println("test2");
+	}
+	
+	@Test
+	public void test3 () {
+		System.out.println("test3");
+	}
+
+}
